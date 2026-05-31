83d2bd9123a1cac91afc6b052b60ca2cd43aadf7
Chantal Höller-Holtrichter <chantal.holler-holtrichterstudent.ap.be>
2026-05-31 02:19:01 +0200

    static geändert

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const app : Express = express();
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(process.cwd(), "public")));
 app.set("views", path.join(process.cwd(), "views"));
 
 
