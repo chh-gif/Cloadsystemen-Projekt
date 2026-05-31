730bb60419bd9e889ecb3c1d5389394b96bc256d
Chantal Höller-Holtrichter <chantal.holler-holtrichterstudent.ap.be>
2026-05-31 02:05:04 +0200

    viewspath ändern

--- index.ts
+++ index.ts
@@ -10,7 +10,8 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, "views"));
+app.set("views", path.join(process.cwd(), "views"));
+
 
 app.set("port", process.env.PORT || 3000);
 
